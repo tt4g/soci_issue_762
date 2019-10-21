68faa17ab53da859c878e4749a6c610fb2cfe3e2
tt4g <mainlinett@gmail.com>
2019-10-21 14:03:39 +0900

    Fixes build soci
    Since soci generates soci/soci-config.h in ${CMAKE_CURRENT_BINARY_DIR}/include, CMake needs to execute in-source-build.

--- soci_issue_762/main.cpp
+++ soci_issue_762/main.cpp
@@ -1,9 +1,14 @@
 #include <soci/soci.h>
+#include <soci/empty/soci-empty.h>
 
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_sinks.h>
 
 int main(int, char**) {
+    soci::session sql(*soci::factory_empty(), "");
+
+    sql << "statement.";
+
     auto logger = spdlog::stdout_logger_mt("stdout");
 
     logger->info("Hello, World!");
